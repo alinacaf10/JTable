7c3ef1b079db56017a2c70b094f3620389d8de61
FARHAD  NAJAFOV <farhadnajafov98.com>
2022-10-11 13:03:31 +0400

    Scroll pane added

--- src/main/Main.java
+++ src/main/Main.java
@@ -6,13 +6,16 @@ public class Main {
 
     public static void main(String[] args) {
         JFrame frame=new JFrame();
-        String[][] data = {{"1","Math","85"},{"2","Geo","90"}};
+        String[][] data = {{"1","Math","85"},{"2","Geo","90"},{"3","Eng","51"}};
         String[] heading={"ID","Lesson","Result"};
         JTable jt=new JTable(data,heading);
         jt.setBounds(30,40,200,300);
+
+        JScrollPane sp=new JScrollPane(jt);
+
+
         frame.setVisible(true);
         frame.setSize(400,400);
-        frame.setLayout(null);
-        frame.add(jt);
+        frame.add(sp);
     }
 }
